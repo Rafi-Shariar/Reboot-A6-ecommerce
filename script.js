8b8dcaa440c65bcb69fd4b20d1b8640af8ab5276
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-02-17 00:36:07 +0600

    trending section done

---
+++ script.js
@@ -0,0 +1,54 @@
+async function loadTrendingProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const allProducts = await res.json();
+
+  // console.log(allProducts);
+
+  const topProducts = allProducts.slice(0, 3);
+  // console.log(topProducts);
+
+  const container = document.getElementById("trending-container");
+  container.innerHTML = "";
+
+  topProducts.forEach((product) => {
+    const card = document.createElement("div");
+    card.className = "h-full";
+
+    card.innerHTML = `
+         <div class="card bg-base-100 shadow-lg h-full flex flex-col">
+            <figure class="border-b-2 border-b-blue-100 py-4 shrink-0 bg-white">
+              <img src="${product.image}" class="object-contain h-48 w-full" />
+            </figure>
+            
+            <div class="card-body flex flex-col p-5 bg-blue-50">
+              <h2 class="card-title text-xl">
+                ${product.title}
+              </h2>
+              
+              
+              <p class="text-sm text-gray-500">
+                ${product.description.slice(0,100)}...
+              </p>
+              
+              <div class="card-actions justify-between items-center mt-auto pt-4 ">
+                <div>
+                <div class="badge badge-outline mr-4">
+                    <i class="fa-solid fa-layer-group text-green-600"></i> ${product.category}
+                </div>
+                <div class="badge badge-outline">
+                    <i class="fa-solid fa-star text-orange-400"></i> ${product.rating.rate}
+                </div>
+                </div>
+
+                <div>
+                    <h1 class="text-xl font-semibold bg-blue-700 p-2 rounded-lg text-white">${product.price} $</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+    container.appendChild(card);
+  });
+}
+
+loadTrendingProducts();
